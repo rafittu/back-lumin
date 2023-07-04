2252b1e04a4917cf12671892e60646c8025af596
rafittu <rafittu@outlook.com>
2023-07-04 10:53:12 -0300

    feat: create admin user controller

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -9,45 +9,45 @@ import {
   Query,
   UseFilters,
 } from '@nestjs/common';
-import { UserService } from './services/user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { UserFilter } from './interfaces/user.interface';
 import { HttpExceptionFilter } from '../../common/filter/http-exception.filter';
 import { AppError } from '../../common/errors/Error';
+import { CreateAdminUserService } from './services/user-admin.service';
 
 @Controller('user')
 @UseFilters(new HttpExceptionFilter(new AppError()))
 export class UserController {
-  constructor(private readonly userService: UserService) {}
+  constructor(private readonly adminUserService: CreateAdminUserService) {}
 
   @Post('/admin')
   createAdminUser(@Body() createUserDto: CreateUserDto) {
-    return this.userService.create(createUserDto);
+    return this.adminUserService.create(createUserDto);
   }
 
-  @Post('/client')
-  createClientUser(@Body() createUserDto: CreateUserDto) {
-    return this.userService.create(createUserDto);
-  }
+  // @Post('/client')
+  // createClientUser(@Body() createUserDto: CreateUserDto) {
+  //   return this.userService.create(createUserDto);
+  // }
 
-  @Get()
-  findAllByFilter(@Query('filter') filter: UserFilter) {
-    return this.userService.findAll();
-  }
+  // @Get()
+  // findAllByFilter(@Query('filter') filter: UserFilter) {
+  //   return this.userService.findAll();
+  // }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.userService.findOne(+id);
-  }
+  // @Get(':id')
+  // findOne(@Param('id') id: string) {
+  //   return this.userService.findOne(+id);
+  // }
 
-  @Patch('/update/:id')
-  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-    return this.userService.update(+id, updateUserDto);
-  }
+  // @Patch('/update/:id')
+  // update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+  //   return this.userService.update(+id, updateUserDto);
+  // }
 
-  @Delete('/delete/:id')
-  remove(@Param('id') id: string) {
-    return this.userService.remove(+id);
-  }
+  // @Delete('/delete/:id')
+  // remove(@Param('id') id: string) {
+  //   return this.userService.remove(+id);
+  // }
 }
