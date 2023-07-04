1365ec687543f27dc9e00097c014500c6396f0c5
rafittu <rafittu@outlook.com>
2023-07-04 10:43:20 -0300

    feat: create user dto

--- src/modules/user/dto/create-user.dto.ts
+++ src/modules/user/dto/create-user.dto.ts
@@ -1,1 +1,28 @@
-export class CreateUserDto {}
+import { IsIn, IsNotEmpty, IsString } from 'class-validator';
+
+export class CreateUserDto {
+  firstName: string;
+
+  lastName: string;
+
+  socialName: string;
+
+  bornDate: string;
+
+  motherName: string;
+
+  username: string;
+
+  email: string;
+
+  phone: string;
+
+  password: string;
+
+  passwordConfirmation: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @IsIn(['lumin'])
+  origin: string;
+}
