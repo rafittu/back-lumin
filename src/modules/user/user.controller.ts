30eeec3d9d43136148c1b1af75af9656a45b7e59
rafittu <rafittu@outlook.com>
2023-07-06 18:12:39 -0300

    sparkles: create client user

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -4,21 +4,25 @@ import { User } from './interfaces/user.interface';
 import { HttpExceptionFilter } from '../../common/filter/http-exception.filter';
 import { AppError } from '../../common/errors/Error';
 import { CreateAdminUserService } from './services/user-admin.service';
+import { CreateClientUserService } from './services/user-client.service';
 
 @Controller('user')
 @UseFilters(new HttpExceptionFilter(new AppError()))
 export class UserController {
-  constructor(private readonly adminUserService: CreateAdminUserService) {}
+  constructor(
+    private readonly adminUserService: CreateAdminUserService,
+    private readonly clientUserService: CreateClientUserService,
+  ) {}
 
   @Post('/admin')
   createAdminUser(@Body() createUserDto: CreateUserDto): Promise<User> {
     return this.adminUserService.execute(createUserDto);
   }
 
-  // @Post('/client')
-  // createClientUser(@Body() createUserDto: CreateUserDto) {
-  //   return this.userService.create(createUserDto);
-  // }
+  @Post('/client')
+  createClientUser(@Body() createUserDto: CreateUserDto) {
+    return this.clientUserService.execute(createUserDto);
+  }
 
   // @Get()
   // findAllByFilter(@Query('filter') filter: UserFilter) {
--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -3,9 +3,15 @@ import { UserController } from './user.controller';
 import { PrismaService } from 'src/prisma.service';
 import { UserRepository } from './repository/user.repository';
 import { CreateAdminUserService } from './services/user-admin.service';
+import { CreateClientUserService } from './services/user-client.service';
 
 @Module({
   controllers: [UserController],
-  providers: [PrismaService, UserRepository, CreateAdminUserService],
+  providers: [
+    PrismaService,
+    UserRepository,
+    CreateAdminUserService,
+    CreateClientUserService,
+  ],
 })
 export class UserModule {}
