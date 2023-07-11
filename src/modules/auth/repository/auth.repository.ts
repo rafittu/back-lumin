7f101a47dd8bc58d087b667a0b9b9df03d328594
rafittu <rafittu@outlook.com>
2023-07-11 15:13:31 -0300

    feat: signin repository

---
+++ src/modules/auth/repository/auth.repository.ts
@@ -0,0 +1,47 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../../../prisma.service';
+import axios from 'axios';
+import { IAuthRepository } from '../interfaces/repository.interface';
+import { AppError } from '../../../common/errors/Error';
+
+@Injectable()
+export class AuthRepository implements IAuthRepository {
+  constructor(private prisma: PrismaService) {}
+
+  private async almaPostRequest(path: string, body: object) {
+    try {
+      const response = await axios.post(path, body);
+      return response.data;
+    } catch (error) {
+      const { status, code, message } = error.response.data.error;
+      throw new AppError(status, code, message);
+    }
+  }
+
+  async signIn(credentialsDto) {
+    const signInPath: string = process.env.SIGNIN_PATH;
+
+    try {
+      const accessToken = await this.almaPostRequest(
+        signInPath,
+        credentialsDto,
+      );
+
+      // get almaId from accessToken
+
+      // get user role by almaId
+
+      return accessToken;
+    } catch (error) {
+      if (error instanceof AppError) {
+        throw new AppError('auth-repository.signIn', 500, error.message);
+      }
+
+      throw new AppError(
+        'auth-repository.signIn',
+        500,
+        'internal server error',
+      );
+    }
+  }
+}
