4de2c65920523d92777d6ef14edde1cec866d70c
rafittu <rafittu@outlook.com>
2023-07-11 19:56:18 -0300

    feat: import redis cache service

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -4,9 +4,16 @@ import { SignInService } from './services/signin.service';
 import { AuthRepository } from './repository/auth.repository';
 import { PrismaService } from 'src/prisma.service';
 import { JwtStrategy } from './infra/strategies/jwt.strategy';
+import { RedisCacheService } from './infra/cache/redis-cache.service';
 
 @Module({
   controllers: [AuthController],
-  providers: [PrismaService, AuthRepository, JwtStrategy, SignInService],
+  providers: [
+    PrismaService,
+    AuthRepository,
+    JwtStrategy,
+    RedisCacheService,
+    SignInService,
+  ],
 })
 export class AuthModule {}
