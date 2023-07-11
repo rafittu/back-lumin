df3b92d63cd5aa4ad265d7f15b21a5a1ff2f9114
rafittu <rafittu@outlook.com>
2023-07-11 15:14:55 -0300

    feat: update module providers

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AuthService } from './services/auth.service';
 import { AuthController } from './auth.controller';
+import { SignInService } from './services/signin.service';
+import { AuthRepository } from './repository/auth.repository';
+import { PrismaService } from 'src/prisma.service';
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [PrismaService, AuthRepository, SignInService],
 })
 export class AuthModule {}
