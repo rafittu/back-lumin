4f9918233d6f15db4eb5b4824257ffc906686716
rafittu <rafittu@outlook.com>
2023-07-13 14:45:08 -0300

    fix: catch authentication errors

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,8 +1,11 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, UseFilters } from '@nestjs/common';
 import { SignInService } from './services/signin.service';
 import { isPublic } from 'src/modules/auth/infra/decorators/is-public.decorator';
 import { JwtToken, UserCredentials } from './interfaces/auth.interface';
+import { HttpExceptionFilter } from 'src/common/filter/http-exception.filter';
+import { AppError } from 'src/common/errors/Error';
 
+@UseFilters(new HttpExceptionFilter(new AppError()))
 @Controller('auth')
 export class AuthController {
   constructor(private readonly signInService: SignInService) {}
