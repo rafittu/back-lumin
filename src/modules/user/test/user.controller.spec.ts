b79600a04c73b3459725b683dfeb0193895aa744
rafittu <rafittu@outlook.com>
2023-07-14 16:24:32 -0300

    test: controller should be defined

--- src/modules/user/test/user.controller.spec.ts
+++ src/modules/user/test/user.controller.spec.ts
@@ -1,17 +1,69 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserController } from '../user.controller';
-import { UserService } from '../services/user.service';
+import { CreateAdminUserService } from '../services/user-admin.service';
+import { CreateClientUserService } from '../services/user-client.service';
+import { GetUserService } from '../services/get-user.service';
+import { GetClientsService } from '../services/get-clients.service';
+import { UpdateUserService } from '../services/update-user.service';
+import { RedisCacheService } from '../../../modules/auth/infra/cache/redis-cache.service';
 
 describe('UserController', () => {
   let controller: UserController;
 
+  let createAdminService: CreateAdminUserService;
+  let createClientService: CreateClientUserService;
+  let getUserService: GetUserService;
+  let getClientsService: GetClientsService;
+  let updateUserService: UpdateUserService;
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UserController],
-      providers: [UserService],
+      providers: [
+        RedisCacheService,
+        {
+          provide: CreateAdminUserService,
+          useValue: {
+            execute: jest.fn(),
+          },
+        },
+        {
+          provide: CreateClientUserService,
+          useValue: {
+            execute: jest.fn(),
+          },
+        },
+        {
+          provide: GetUserService,
+          useValue: {
+            execute: jest.fn(),
+          },
+        },
+        {
+          provide: GetClientsService,
+          useValue: {
+            execute: jest.fn(),
+          },
+        },
+        {
+          provide: UpdateUserService,
+          useValue: {
+            execute: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     controller = module.get<UserController>(UserController);
+    createAdminService = module.get<CreateAdminUserService>(
+      CreateAdminUserService,
+    );
+    createClientService = module.get<CreateClientUserService>(
+      CreateClientUserService,
+    );
+    getUserService = module.get<GetUserService>(GetUserService);
+    getClientsService = module.get<GetClientsService>(GetClientsService);
+    updateUserService = module.get<UpdateUserService>(UpdateUserService);
   });
 
   it('should be defined', () => {
