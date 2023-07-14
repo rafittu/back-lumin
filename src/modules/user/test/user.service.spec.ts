1f58a1fbd04d027968f523d56e053bc37464be7e
rafittu <rafittu@outlook.com>
2023-07-14 19:31:42 -0300

    test: services should be defined

--- src/modules/user/test/user.service.spec.ts
+++ src/modules/user/test/user.service.spec.ts
@@ -1,18 +1,56 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { UserService } from '../services/user.service';
+import { CreateAdminUserService } from '../services/user-admin.service';
+import { CreateClientUserService } from '../services/user-client.service';
+import { GetUserService } from '../services/get-user.service';
+import { GetClientsService } from '../services/get-clients.service';
+import { UpdateUserService } from '../services/update-user.service';
+import { UserRepository } from '../repository/user.repository';
 
-describe('UserService', () => {
-  let service: UserService;
+describe('UserServices', () => {
+  let createAdminService: CreateAdminUserService;
+  let createClientService: CreateClientUserService;
+  let getUserService: GetUserService;
+  let getClientsService: GetClientsService;
+  let updateUserService: UpdateUserService;
+
+  let userRepository: UserRepository;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UserService],
+      providers: [
+        CreateAdminUserService,
+        CreateClientUserService,
+        GetUserService,
+        GetClientsService,
+        UpdateUserService,
+        {
+          provide: UserRepository,
+          useValue: {
+            createUser: jest.fn(),
+            getUser: jest.fn(),
+            getClients: jest.fn(),
+            updateUser: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
-    service = module.get<UserService>(UserService);
+    createAdminService = module.get<CreateAdminUserService>(
+      CreateAdminUserService,
+    );
+    createClientService = module.get<CreateClientUserService>(
+      CreateClientUserService,
+    );
+    getUserService = module.get<GetUserService>(GetUserService);
+    getClientsService = module.get<GetClientsService>(GetClientsService);
+    updateUserService = module.get<UpdateUserService>(UpdateUserService);
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(createAdminService).toBeDefined();
+    expect(createClientService).toBeDefined();
+    expect(getUserService).toBeDefined();
+    expect(getClientsService).toBeDefined();
+    expect(updateUserService).toBeDefined();
   });
 });
