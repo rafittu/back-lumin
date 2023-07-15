37a111510b6c686a016e468e92a638cef247ca98
rafittu <rafittu@outlook.com>
2023-07-15 16:28:00 -0300

    test: create user successfully

--- src/modules/user/test/user.repository.spec.ts
+++ src/modules/user/test/user.repository.spec.ts
@@ -1,6 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PrismaService } from '../../../prisma.service';
 import { UserRepository } from '../repository/user.repository';
+import {
+  mockAlmaUser,
+  mockNewUser,
+  mockPrismaCreatedUser,
+} from './mocks/repository.mock';
+import { UserRole } from '../enum/user-role.enum';
+import { mockCreateUserBody } from './mocks/controller.mock';
 
 describe('UserRepository', () => {
   let userRepository: UserRepository;
@@ -17,5 +24,26 @@ describe('UserRepository', () => {
 
   it('should be defined', () => {
     expect(userRepository).toBeDefined();
+  });
+
+  describe('createUser', () => {
+    it('should create a new user successfully', async () => {
+      jest
+        .spyOn(userRepository as any, 'almaPostRequest')
+        .mockResolvedValueOnce(mockAlmaUser);
+
+      jest
+        .spyOn(prismaService.user, 'create')
+        .mockResolvedValueOnce(mockPrismaCreatedUser);
+
+      const result = await userRepository.createUser(
+        mockCreateUserBody,
+        UserRole.CLIENT,
+      );
+
+      expect(userRepository['almaPostRequest']).toHaveBeenCalledTimes(1);
+      expect(prismaService.user.create).toHaveBeenCalledTimes(1);
+      expect(result).toEqual(mockNewUser);
+    });
   });
 });
