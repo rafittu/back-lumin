6d69c9690d531429af93aa363f384d32354138b0
rafittu <rafittu@outlook.com>
2023-07-17 19:41:59 -0300

    test: setup auth service test

--- src/modules/auth/test/auth.service.spec.ts
+++ src/modules/auth/test/auth.service.spec.ts
@@ -1,18 +1,31 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { AuthService } from '../services/auth.service';
+import { SignInService } from '../services/signin.service';
+import { AuthRepository } from '../repository/auth.repository';
 
 describe('AuthService', () => {
-  let service: AuthService;
+  let signInService: SignInService;
+
+  let authRepository: AuthRepository;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      providers: [
+        SignInService,
+        {
+          provide: AuthRepository,
+          useValue: {
+            signIn: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
-    service = module.get<AuthService>(AuthService);
+    signInService = module.get<SignInService>(SignInService);
+
+    authRepository = module.get<AuthRepository>(AuthRepository);
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(signInService).toBeDefined();
   });
 });
