ad7060ed7077c97e66d36f6b9982004a2204078e
rafittu <rafittu@outlook.com>
2023-07-17 20:01:44 -0300

    test: setup auth repository test

---
+++ src/modules/auth/test/auth.repository.spec.ts
@@ -0,0 +1,23 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { Test, TestingModule } from '@nestjs/testing';
+import { PrismaService } from '../../../prisma.service';
+import { AuthRepository } from '../repository/auth.repository';
+import { RedisCacheService } from '../infra/cache/redis-cache.service';
+
+describe('UserRepository', () => {
+  let authRepository: AuthRepository;
+  let prismaService: PrismaService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [AuthRepository, PrismaService, RedisCacheService],
+    }).compile();
+
+    authRepository = module.get<AuthRepository>(AuthRepository);
+    prismaService = module.get<PrismaService>(PrismaService);
+  });
+
+  it('should be defined', () => {
+    expect(authRepository).toBeDefined();
+  });
+});
