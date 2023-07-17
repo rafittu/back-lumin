9372f73522fecf1da92f0f28f892774399352846
rafittu <rafittu@outlook.com>
2023-07-17 19:24:07 -0300

    test: setup auth controller test

--- src/modules/auth/test/auth.controller.spec.ts
+++ src/modules/auth/test/auth.controller.spec.ts
@@ -1,17 +1,29 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthController } from '../auth.controller';
-import { AuthService } from '../services/auth.service';
+import { SignInService } from '../services/signin.service';
+import { RedisCacheService } from '../../../modules/auth/infra/cache/redis-cache.service';
 
 describe('AuthController', () => {
   let controller: AuthController;
 
+  let signInService: SignInService;
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AuthController],
-      providers: [AuthService],
+      providers: [
+        RedisCacheService,
+        {
+          provide: SignInService,
+          useValue: {
+            execute: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     controller = module.get<AuthController>(AuthController);
+    signInService = module.get<SignInService>(SignInService);
   });
 
   it('should be defined', () => {
