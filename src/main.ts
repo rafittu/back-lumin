3ad9c80c6b01249f1deb296fd57fbb297fea315e
rafittu <rafittu@outlook.com>
2023-07-18 11:43:01 -0300

    feat: setup swagger server

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import helmet from 'helmet';
 import { PrismaService } from './prisma.service';
+import * as swaggerUi from 'swagger-ui-express';
+import * as SwaggerDoc from '../swagger.json';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -10,6 +12,8 @@ async function bootstrap() {
   app.use(helmet());
   app.use(helmet.hidePoweredBy());
   app.use(helmet.contentSecurityPolicy());
+
+  app.use('/api-doc', swaggerUi.serve, swaggerUi.setup(SwaggerDoc));
 
   const prismaService = app.get(PrismaService);
   await prismaService.enableShutdownHooks(app);
