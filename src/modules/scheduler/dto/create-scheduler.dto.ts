3c1a578b3a957b0b60359b4c2c7722cf5a377b92
rafittu <rafittu@outlook.com>
2023-07-20 11:26:25 -0300

    feat: create appointmment dto

--- src/modules/scheduler/dto/create-scheduler.dto.ts
+++ src/modules/scheduler/dto/create-scheduler.dto.ts
@@ -1,1 +1,15 @@
-export class CreateSchedulerDto {}
+import { IsNotEmpty, IsString } from 'class-validator';
+
+export class CreateAppointmmentDto {
+  @IsNotEmpty()
+  @IsString()
+  clientName: string;
+
+  @IsNotEmpty()
+  @IsString()
+  clientPhone: string;
+
+  @IsNotEmpty()
+  @IsString()
+  scheduledAt: Date;
+}
