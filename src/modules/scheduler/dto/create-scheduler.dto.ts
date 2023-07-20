be537c6679fa7ba8360ff5378c291ad8b8fe8648
rafittu <rafittu@outlook.com>
2023-07-20 11:55:24 -0300

    fix: validate appt date

--- src/modules/scheduler/dto/create-scheduler.dto.ts
+++ src/modules/scheduler/dto/create-scheduler.dto.ts
@@ -1,4 +1,10 @@
-import { IsNotEmpty, IsString } from 'class-validator';
+import {
+  IsDateString,
+  IsNotEmpty,
+  IsString,
+  Matches,
+  MaxLength,
+} from 'class-validator';
 
 export class CreateAppointmmentDto {
   @IsNotEmpty()
@@ -10,6 +16,13 @@ export class CreateAppointmmentDto {
   clientPhone: string;
 
   @IsNotEmpty()
-  @IsString()
-  scheduledAt: Date;
+  @IsDateString()
+  @MaxLength(10, { message: 'must be a valid date: yyyy-mm-dd' })
+  @Matches(
+    /^([0-9]{4}-((0[13578]|1[02])-([0-2][1-9]|3[0-1])|(0[469]|11)-(0[1-9]|[1-2][0-9]|30)|02-(0[1-9]|[1-2][0-9]))|([0-9]{2}(([2468][048]|[02468][48])|[13579][26])|([13579][26]|[02468][048]|0[0-9]|1[0-6])00)-02-29)$/,
+    {
+      message: 'must be a valid date and formatted as yyyy-mm-dd',
+    },
+  )
+  scheduledAt: string;
 }
