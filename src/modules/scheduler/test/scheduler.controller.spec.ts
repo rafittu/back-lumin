f9724416fd9239875f31e5b8f81c39090803e948
rafittu <rafittu@outlook.com>
2023-07-20 17:55:01 -0300

    test: setup controller test

--- src/modules/scheduler/test/scheduler.controller.spec.ts
+++ src/modules/scheduler/test/scheduler.controller.spec.ts
@@ -1,16 +1,32 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { SchedulerController } from '../scheduler.controller';
+import { RedisCacheService } from '../../../modules/auth/infra/cache/redis-cache.service';
+import { CreateAppointmentService } from '../services/create-appt.service';
 
 describe('SchedulerController', () => {
   let controller: SchedulerController;
 
+  let createAppointmentService: CreateAppointmentService;
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [SchedulerController],
-      providers: [],
+      providers: [
+        RedisCacheService,
+        {
+          provide: CreateAppointmentService,
+          useValue: {
+            execute: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     controller = module.get<SchedulerController>(SchedulerController);
+
+    createAppointmentService = module.get<CreateAppointmentService>(
+      CreateAppointmentService,
+    );
   });
 
   it('should be defined', () => {
