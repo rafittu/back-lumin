ad8cec1f8f9dcc626585af4069c8d33fdbcee1c5
rafittu <rafittu@outlook.com>
2023-07-20 18:15:35 -0300

    test: [service] create appt successfully

--- src/modules/scheduler/test/scheduler.service.spec.ts
+++ src/modules/scheduler/test/scheduler.service.spec.ts
@@ -1,15 +1,50 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { CreateAppointmentService } from '../services/create-appt.service';
+import { SchedulerRepository } from '../repository/scheduler.repository';
+import { mockNewAppointment } from './mocks/common.mock';
+import {
+  mockCreateAppointment,
+  mockProfessionalId,
+} from './mocks/controller.mock';
 
 describe('SchedulerService', () => {
-  let service;
+  let createAppointmentService: CreateAppointmentService;
+
+  let schedulerRepository: SchedulerRepository;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [],
+      providers: [
+        CreateAppointmentService,
+        {
+          provide: SchedulerRepository,
+          useValue: {
+            createAppointment: jest.fn().mockResolvedValue(mockNewAppointment),
+          },
+        },
+      ],
     }).compile();
+
+    createAppointmentService = module.get<CreateAppointmentService>(
+      CreateAppointmentService,
+    );
+
+    schedulerRepository = module.get<SchedulerRepository>(SchedulerRepository);
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(createAppointmentService).toBeDefined();
+  });
+
+  describe('create appointment', () => {
+    it('should create a new one successfully', async () => {
+      const result = await createAppointmentService.execute(
+        mockProfessionalId,
+        mockCreateAppointment,
+      );
+
+      expect(schedulerRepository.createAppointment).toHaveBeenCalledTimes(1);
+      expect(result).toEqual(mockNewAppointment);
+    });
   });
 });
