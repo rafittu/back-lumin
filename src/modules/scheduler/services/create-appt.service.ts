5ae55b3f5174a9c5b52d974fc998f70484b45b59
rafittu <rafittu@outlook.com>
2023-07-20 17:21:12 -0300

    feat: catch missing parameter error

--- src/modules/scheduler/services/create-appt.service.ts
+++ src/modules/scheduler/services/create-appt.service.ts
@@ -3,6 +3,7 @@ import { ISchedulerRepository } from '../interfaces/repository.interface';
 import { SchedulerRepository } from '../repository/scheduler.repository';
 import { CreateAppointmentDto } from '../dto/create-scheduler.dto';
 import { NewAppointment } from '../interfaces/appointment.interface';
+import { AppError } from 'src/common/errors/Error';
 
 @Injectable()
 export class CreateAppointmentService {
@@ -15,6 +16,13 @@ export class CreateAppointmentService {
     professionalId: string,
     createAppointmentDto: CreateAppointmentDto,
   ): Promise<NewAppointment> {
+    if (!professionalId) {
+      throw new AppError(
+        'appt-module.createAppointment',
+        400,
+        'missing query parameter [professionalId]',
+      );
+    }
     return await this.schedulerRepository.createAppointment(
       professionalId,
       createAppointmentDto,
