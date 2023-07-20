061e5af5eb98974cccc36738f38e0e75bdab8062
rafittu <rafittu@outlook.com>
2023-07-20 17:17:19 -0300

    fix: change appointmentDate type

--- src/modules/scheduler/dto/create-scheduler.dto.ts
+++ src/modules/scheduler/dto/create-scheduler.dto.ts
@@ -1,10 +1,4 @@
-import {
-  IsDateString,
-  IsNotEmpty,
-  IsString,
-  Matches,
-  MaxLength,
-} from 'class-validator';
+import { IsNotEmpty, IsString, Matches, MaxLength } from 'class-validator';
 
 export class CreateAppointmentDto {
   @IsNotEmpty()
@@ -16,7 +10,7 @@ export class CreateAppointmentDto {
   clientPhone: string;
 
   @IsNotEmpty()
-  @IsDateString()
+  @IsString()
   @MaxLength(10, { message: 'must be a valid date: yyyy-mm-dd' })
   @Matches(
     /^([0-9]{4}-((0[13578]|1[02])-([0-2][1-9]|3[0-1])|(0[469]|11)-(0[1-9]|[1-2][0-9]|30)|02-(0[1-9]|[1-2][0-9]))|([0-9]{2}(([2468][048]|[02468][48])|[13579][26])|([13579][26]|[02468][048]|0[0-9]|1[0-6])00)-02-29)$/,
@@ -24,7 +18,7 @@ export class CreateAppointmentDto {
       message: 'must be a valid date and formatted as yyyy-mm-dd',
     },
   )
-  appointmentDate: Date;
+  appointmentDate: string;
 
   @IsNotEmpty()
   @IsString()
