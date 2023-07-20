73f6f1244c087888bd13b1e6021869e7f3abdfcc
rafittu <rafittu@outlook.com>
2023-07-20 16:16:57 -0300

    fire: remove unused code

--- src/modules/scheduler/dto/update-scheduler.dto.ts
+++ src/modules/scheduler/dto/update-scheduler.dto.ts
@@ -1,4 +1,3 @@
 import { PartialType } from '@nestjs/mapped-types';
-import { CreateSchedulerDto } from './create-scheduler.dto';
 
-export class UpdateSchedulerDto extends PartialType(CreateSchedulerDto) {}
+export class UpdateSchedulerDto {}
