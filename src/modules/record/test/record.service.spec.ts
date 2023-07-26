3de0eebd41aad106cf8b687d026077e105b9c293
rafittu <rafittu@outlook.com>
2023-07-26 20:53:36 -0300

    test: [service] create record successfully

--- src/modules/record/test/record.service.spec.ts
+++ src/modules/record/test/record.service.spec.ts
@@ -3,12 +3,22 @@ import { CreateRecordService } from '../services/create-record.service';
 import { RecordRepository } from '../repository/record.repository';
 import { mockRepositoryRecordResponse } from './mocks/repository.mock';
 import { SchedulerRepository } from '../../../modules/scheduler/repository/scheduler.repository';
-import { mockUserAppointment } from './mocks/common.mock';
+import {
+  mockNewRecord,
+  mockProfessionalAppointments,
+  mockUserAppointment,
+} from './mocks/common.mock';
+import {
+  mockAppointmentId,
+  mockCreateRecord,
+  mockProfessionalId,
+} from './mocks/controller.mock';
 
 describe('RecordServices', () => {
   let createRecordService: CreateRecordService;
 
   let recordRepository: RecordRepository;
+  let schedulerRepository: SchedulerRepository;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -34,9 +44,27 @@ describe('RecordServices', () => {
     createRecordService = module.get<CreateRecordService>(CreateRecordService);
 
     recordRepository = module.get<RecordRepository>(RecordRepository);
+    schedulerRepository = module.get<SchedulerRepository>(SchedulerRepository);
   });
 
   it('should be defined', () => {
     expect(createRecordService).toBeDefined();
+  });
+
+  describe('create record', () => {
+    it('should create a new record successfully', async () => {
+      jest
+        .spyOn(schedulerRepository, 'getApptByFilter')
+        .mockResolvedValueOnce(mockProfessionalAppointments);
+
+      const result = await createRecordService.execute(
+        mockProfessionalId,
+        mockAppointmentId,
+        mockCreateRecord,
+      );
+
+      expect(recordRepository.createRecord).toHaveBeenCalledTimes(1);
+      expect(result).toEqual(mockNewRecord);
+    });
   });
 });
