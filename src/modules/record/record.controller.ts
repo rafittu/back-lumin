8f351b9ff7de20c4ae826868abf3535690a4881d
rafittu <rafittu@outlook.com>
2023-07-26 14:40:15 -0300

    feat: create record controller

--- src/modules/record/record.controller.ts
+++ src/modules/record/record.controller.ts
@@ -6,15 +6,37 @@ import {
   Patch,
   Param,
   Delete,
+  UseGuards,
+  UseFilters,
+  Query,
 } from '@nestjs/common';
+import { RolesGuard } from '../auth/infra/guards/role.guard';
+import { HttpExceptionFilter } from 'src/common/filter/http-exception.filter';
+import { AppError } from 'src/common/errors/Error';
+import { Roles } from '../auth/infra/decorators/role.decorator';
+import { UserRole } from '../user/enum/user-role.enum';
 import { CreateRecordDto } from './dto/create-record.dto';
 import { UpdateRecordDto } from './dto/update-record.dto';
+import { CreateRecordService } from './services/create-record.service';
 
+@UseGuards(RolesGuard)
+@UseFilters(new HttpExceptionFilter(new AppError()))
 @Controller('record')
 export class RecordController {
-  @Post()
-  create(@Body() createRecordDto: CreateRecordDto) {
-    return 'record created';
+  constructor(private readonly createRecordService: CreateRecordService) {}
+
+  @Post('/create')
+  @Roles(UserRole.ADMIN)
+  async create(
+    @Query('professionalId') professionalId: string,
+    @Query('scheduleId') scheduleId: string,
+    @Body() record: CreateRecordDto,
+  ) {
+    return await this.createRecordService.execute(
+      professionalId,
+      scheduleId,
+      record,
+    );
   }
 
   @Get()
