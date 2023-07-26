ae3ea6e802bf525bba7d00ed719d549b18b6b0a3
rafittu <rafittu@outlook.com>
2023-07-26 19:44:39 -0300

    test: [repository] create new record successfully

--- src/modules/record/test/record.repository.spec.ts
+++ src/modules/record/test/record.repository.spec.ts
@@ -1,6 +1,12 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PrismaService } from '../../../prisma.service';
 import { RecordRepository } from '../repository/record.repository';
+import {
+  mockPrismaNewRecord,
+  mockRepositoryRecordResponse,
+} from './mocks/repository.mock';
+import { mockAppointmentId, mockProfessionalId } from './mocks/controller.mock';
+import { mockEncryptedRecord } from './mocks/service.mock';
 
 describe('RecordRepository', () => {
   let recordRepository: RecordRepository;
@@ -23,5 +29,22 @@ describe('RecordRepository', () => {
   it('should be defined', () => {
     expect(recordRepository).toBeDefined();
     expect(prismaService).toBeDefined();
+  });
+
+  describe('create record', () => {
+    it('should create a new record successfully', async () => {
+      jest
+        .spyOn(prismaService.appointmentRecord, 'create')
+        .mockResolvedValueOnce(mockPrismaNewRecord);
+
+      const result = await recordRepository.createRecord(
+        mockProfessionalId,
+        mockAppointmentId,
+        mockEncryptedRecord,
+      );
+
+      expect(prismaService.appointmentRecord.create).toHaveBeenCalledTimes(1);
+      expect(result).toEqual(mockRepositoryRecordResponse);
+    });
   });
 });
