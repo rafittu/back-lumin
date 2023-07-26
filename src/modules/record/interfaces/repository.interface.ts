9355cf13b90967a4cd190c7f2660de8b96123611
rafittu <rafittu@outlook.com>
2023-07-26 19:00:03 -0300

    feat: created record interface

--- src/modules/record/interfaces/repository.interface.ts
+++ src/modules/record/interfaces/repository.interface.ts
@@ -1,5 +1,11 @@
 import { CreateRecordDto } from '../dto/create-record.dto';
 
+export interface Record {
+  id: string;
+  professionalId: string;
+  createdAt: Date;
+}
+
 export interface IRecordRepository {
   createRecord(
     professionalId: string,
