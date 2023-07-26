ffacc30207f9c186ec9e7ac0cb55aeb66c1a48bd
rafittu <rafittu@outlook.com>
2023-07-26 19:10:05 -0300

    setup controller test

--- src/modules/record/test/record.controller.spec.ts
+++ src/modules/record/test/record.controller.spec.ts
@@ -1,17 +1,30 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { RecordController } from '../record.controller';
 import { CreateRecordService } from '../services/create-record.service';
+import { RedisCacheService } from '../../../modules/auth/infra/cache/redis-cache.service';
 
 describe('RecordController', () => {
   let controller: RecordController;
 
+  let createRecordService: CreateRecordService;
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [RecordController],
-      providers: [CreateRecordService],
+      providers: [
+        RedisCacheService,
+        {
+          provide: CreateRecordService,
+          useValue: {
+            execute: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     controller = module.get<RecordController>(RecordController);
+
+    createRecordService = module.get<CreateRecordService>(CreateRecordService);
   });
 
   it('should be defined', () => {
