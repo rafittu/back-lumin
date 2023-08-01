8e92ac45a89f91a5747dc3de8203325a202844b5
rafittu <rafittu@outlook.com>
2023-08-01 13:29:24 -0300

    fix: update redord dto

--- src/modules/record/dto/update-record.dto.ts
+++ src/modules/record/dto/update-record.dto.ts
@@ -1,8 +1,6 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateRecordDto } from './create-record.dto';
 import { IsNotEmpty, IsString } from 'class-validator';
 
-export class UpdateRecordDto extends PartialType(CreateRecordDto) {
+export class UpdateRecordDto {
   @IsNotEmpty()
   @IsString()
   record: string;
