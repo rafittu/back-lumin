b44cabe9fbd0d4ac96129d3a9c73b17b5fee81e2
rafittu <rafittu@outlook.com>
2023-08-04 15:35:23 -0300

    create payment service

--- src/modules/payments/services/create-payment.service.ts
+++ src/modules/payments/services/create-payment.service.ts
@@ -2,6 +2,7 @@ import { Inject, Injectable } from '@nestjs/common';
 import { PaymentRepository } from '../repository/payment.repository';
 import { IPaymentRepository } from '../interfaces/repository.interface';
 import { CreatePaymentDto } from '../dto/create-payment.dto';
+import { AppError } from '../../../common/errors/Error';
 
 @Injectable()
 export class CreatePaymentService {
@@ -10,7 +11,23 @@ export class CreatePaymentService {
     private paymentRepository: IPaymentRepository,
   ) {}
 
-  execute(createPaymentDto: CreatePaymentDto) {
-    return this.paymentRepository.createPayment(createPaymentDto);
+  execute(
+    professionalId: string,
+    appointmentId: string,
+    createPaymentDto: CreatePaymentDto,
+  ) {
+    if (!professionalId || !appointmentId) {
+      throw new AppError(
+        'payment-module.createPayment',
+        400,
+        'missing query parameter [professionalId, appointmentId]',
+      );
+    }
+
+    return this.paymentRepository.createPayment(
+      professionalId,
+      appointmentId,
+      createPaymentDto,
+    );
   }
 }
