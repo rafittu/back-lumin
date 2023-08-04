3b301381f367f25e5e653c8d8af6d5ca7be2aaf1
rafittu <rafittu@outlook.com>
2023-08-04 15:27:24 -0300

    feat: create payment dto and enum

--- src/modules/payments/dto/create-payment.dto.ts
+++ src/modules/payments/dto/create-payment.dto.ts
@@ -1,1 +1,38 @@
-export class CreatePaymentDto {}
+import {
+  IsDecimal,
+  IsEnum,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  MaxLength,
+} from 'class-validator';
+import { PaymentStatus } from '../enum/payment-status.enum';
+import { PaymentMethod } from '../enum/payment-method.enum';
+import { Decimal } from '@prisma/client/runtime';
+
+export class CreatePaymentDto {
+  @IsNotEmpty()
+  @IsString()
+  professionalId: string;
+
+  @IsOptional()
+  @IsString()
+  @MaxLength(10, { message: 'must be a valid date: yyyy-mm-dd' })
+  paymentDate?: string;
+
+  @IsOptional()
+  @IsEnum(PaymentMethod)
+  paymentMethod?: PaymentMethod;
+
+  @IsNotEmpty()
+  @IsString()
+  appointmentId: string;
+
+  @IsNotEmpty()
+  @IsEnum(PaymentStatus)
+  status: PaymentStatus;
+
+  @IsOptional()
+  @IsDecimal()
+  totalPaid?: Decimal;
+}
