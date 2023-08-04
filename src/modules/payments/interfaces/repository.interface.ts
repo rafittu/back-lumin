b182b47afcc4f4d7e2fbdff07846c3ab318b512b
rafittu <rafittu@outlook.com>
2023-08-04 15:46:41 -0300

    feat: create payment repository

--- src/modules/payments/interfaces/repository.interface.ts
+++ src/modules/payments/interfaces/repository.interface.ts
@@ -2,7 +2,11 @@ import { CreatePaymentDto } from '../dto/create-payment.dto';
 import { UpdatePaymentDto } from '../dto/update-payment.dto';
 
 export interface IPaymentRepository {
-  createPayment(createPaymentDto: CreatePaymentDto);
+  createPayment(
+    professionalId: string,
+    appointmentId: string,
+    createPaymentDto: CreatePaymentDto,
+  );
   findPaymentByFilter(filter);
   getOnePayment(id);
   updatePayment(paymentId: string, updatePaymentDto: UpdatePaymentDto);
--- src/modules/payments/repository/payment.repository.ts
+++ src/modules/payments/repository/payment.repository.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../../../prisma.service';
-import axios from 'axios';
 import { AppError } from '../../../common/errors/Error';
 import { IPaymentRepository } from '../interfaces/repository.interface';
 import { CreatePaymentDto } from '../dto/create-payment.dto';
@@ -10,11 +9,41 @@ import { UpdatePaymentDto } from '../dto/update-payment.dto';
 export class PaymentRepository implements IPaymentRepository {
   constructor(private prisma: PrismaService) {}
 
-  async createPayment(createPaymentDto: CreatePaymentDto) {
+  async createPayment(
+    professionalId: string,
+    appointmentId: string,
+    createPaymentDto: CreatePaymentDto,
+  ) {
+    const { paymentDate, paymentMethod, status, totalPaid } = createPaymentDto;
+
     try {
-      return 'payment data';
+      const payment = await this.prisma.payment.create({
+        data: {
+          professional_id: professionalId,
+          appointment_id: appointmentId,
+          payment_date: paymentDate,
+          payment_method: paymentMethod,
+          status,
+          total_paid: totalPaid,
+        },
+      });
+
+      const paymentResponse = {
+        id: payment.id,
+        totalPaid,
+        paymentDate,
+        status,
+        createdAt: payment.created_at,
+        updatedAt: payment.updated_at,
+      };
+
+      return paymentResponse;
     } catch (error) {
-      throw new AppError('Not Implemented', 501, 'message');
+      throw new AppError(
+        'payment-module.createPayment',
+        500,
+        'payment not created',
+      );
     }
   }
 
