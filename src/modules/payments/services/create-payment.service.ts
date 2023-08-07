c18b2b91d25892ade7d2774b099cba2e3124a545
rafittu <rafittu@outlook.com>
2023-08-07 11:22:17 -0300

    feat: improve create payment validation

--- src/modules/payments/services/create-payment.service.ts
+++ src/modules/payments/services/create-payment.service.ts
@@ -4,6 +4,7 @@ import { IPaymentRepository } from '../interfaces/repository.interface';
 import { CreatePaymentDto } from '../dto/create-payment.dto';
 import { AppError } from '../../../common/errors/Error';
 import { PaymentResponse } from '../interfaces/payment.interface';
+import { PaymentStatus } from '../enum/payment-status.enum';
 
 @Injectable()
 export class CreatePaymentService {
@@ -23,6 +24,21 @@ export class CreatePaymentService {
         400,
         'missing query parameter [professionalId, appointmentId]',
       );
+    }
+
+    if (createPaymentDto.status === PaymentStatus.PAID) {
+      const requiredFields = ['totalPaid', 'paymentDate', 'paymentMethod'];
+      const missingFields = requiredFields.filter(
+        (field) => !createPaymentDto[field],
+      );
+
+      if (missingFields.length > 0) {
+        throw new AppError(
+          'payment-service.createPayment',
+          400,
+          `missing values for fields: ${missingFields.join(', ')}`,
+        );
+      }
     }
 
     return this.paymentRepository.createPayment(
