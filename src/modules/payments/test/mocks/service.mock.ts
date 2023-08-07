e4debe0df44fff0f0720730f9a2d80f1e9235bf7
rafittu <rafittu@outlook.com>
2023-08-07 14:11:48 -0300

    test: [service] create payment successfully

---
+++ src/modules/payments/test/mocks/service.mock.ts
@@ -0,0 +1,25 @@
+import { faker } from '@faker-js/faker';
+import { PaymentResponse } from '../../interfaces/payment.interface';
+import { CreatePaymentDto } from '../../dto/create-payment.dto';
+import { PaymentMethod } from '../../enum/payment-method.enum';
+import { PaymentStatus } from '../../enum/payment-status.enum';
+
+export const mockProfessionalId = faker.string.uuid();
+
+export const mockAppointmentId = faker.string.uuid();
+
+export const mockCreatePayment: CreatePaymentDto = {
+  paymentDate: faker.date.recent().toISOString().slice(0, 10),
+  paymentMethod: PaymentMethod.PIX,
+  status: PaymentStatus.PAID,
+  totalPaid: faker.commerce.price(),
+};
+
+export const mockPaymentResponse: PaymentResponse = {
+  id: faker.string.uuid(),
+  paymentDate: mockCreatePayment.paymentDate,
+  status: mockCreatePayment.status,
+  totalPaid: mockCreatePayment.totalPaid,
+  createdAt: faker.date.recent(),
+  updatedAt: faker.date.recent(),
+};
--- src/modules/payments/test/payments.service.spec.ts
+++ src/modules/payments/test/payments.service.spec.ts
@@ -1,13 +1,67 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { CreatePaymentService } from '../services/create-payment.service';
+import { FindPaymentByFilterService } from '../services/find-by-filter.service';
+import { GetOnePaymentService } from '../services/get-one-payment.service';
+import { UpdatePaymentService } from '../services/update-payment.service';
+import { PaymentRepository } from '../repository/payment.repository';
+import {
+  mockAppointmentId,
+  mockCreatePayment,
+  mockPaymentResponse,
+  mockProfessionalId,
+} from './mocks/service.mock';
 
 describe('PaymentsService', () => {
+  let createPaymentService: CreatePaymentService;
+  let findPaymentByFilterService: FindPaymentByFilterService;
+  let findOnePaymentService: GetOnePaymentService;
+  let updatePaymentService: UpdatePaymentService;
+
+  let paymentRepository: PaymentRepository;
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [],
+      providers: [
+        CreatePaymentService,
+        FindPaymentByFilterService,
+        GetOnePaymentService,
+        UpdatePaymentService,
+        {
+          provide: PaymentRepository,
+          useValue: {
+            createPayment: jest.fn().mockResolvedValue(mockPaymentResponse),
+          },
+        },
+      ],
     }).compile();
+
+    createPaymentService =
+      module.get<CreatePaymentService>(CreatePaymentService);
+    findPaymentByFilterService = module.get<FindPaymentByFilterService>(
+      FindPaymentByFilterService,
+    );
+    findOnePaymentService =
+      module.get<GetOnePaymentService>(GetOnePaymentService);
+    updatePaymentService =
+      module.get<UpdatePaymentService>(UpdatePaymentService);
+
+    paymentRepository = module.get<PaymentRepository>(PaymentRepository);
   });
 
   it('should be defined', () => {
-    expect('service').toBeDefined();
+    expect(createPaymentService).toBeDefined();
+  });
+
+  describe('create payment', () => {
+    it('should create a new payment successfully', async () => {
+      const result = await createPaymentService.execute(
+        mockProfessionalId,
+        mockAppointmentId,
+        mockCreatePayment,
+      );
+
+      expect(paymentRepository.createPayment).toHaveBeenCalledTimes(1);
+      expect(result).toEqual(mockPaymentResponse);
+    });
   });
 });
