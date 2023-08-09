cf004507f66e19d1b2fe4892a2c2452e62d2ca8d
rafittu <rafittu@outlook.com>
2023-08-09 16:07:06 -0300

    test: [repository] find payments by clientName

--- src/modules/payments/test/payments.repository.spec.ts
+++ src/modules/payments/test/payments.repository.spec.ts
@@ -198,5 +198,19 @@ describe('PaymentRepository', () => {
       expect(prismaService.payment.findMany).toHaveBeenCalledTimes(1);
       expect(result).toEqual(mockPaymentsByFilter);
     });
+
+    it('should get payment by clientName successfully', async () => {
+      jest
+        .spyOn(prismaService.payment, 'findMany')
+        .mockResolvedValueOnce(mockPrismaPaymentByFilterResponse);
+
+      const result = await paymentRepository.findPaymentByFilter(
+        mockProfessionalId,
+        { clientName: mockGetPaymentFilter.clientName },
+      );
+
+      expect(prismaService.payment.findMany).toHaveBeenCalledTimes(1);
+      expect(result).toEqual(mockPaymentsByFilter);
+    });
   });
 });
