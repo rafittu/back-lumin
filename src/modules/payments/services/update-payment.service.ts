82a04be508729e10bae08cb3bed7ad4d69f252dd
rafittu <rafittu@outlook.com>
2023-08-11 10:33:42 -0300

    feat: update payment service

--- src/modules/payments/services/update-payment.service.ts
+++ src/modules/payments/services/update-payment.service.ts
@@ -2,6 +2,8 @@ import { Inject, Injectable } from '@nestjs/common';
 import { PaymentRepository } from '../repository/payment.repository';
 import { IPaymentRepository } from '../interfaces/repository.interface';
 import { UpdatePaymentDto } from '../dto/update-payment.dto';
+import { PaymentStatus } from '../enum/payment-status.enum';
+import { AppError } from '../../../common/errors/Error';
 
 @Injectable()
 export class UpdatePaymentService {
@@ -10,7 +12,42 @@ export class UpdatePaymentService {
     private paymentRepository: IPaymentRepository,
   ) {}
 
-  execute(paymentId: string, updatePaymentDto: UpdatePaymentDto) {
+  async execute(paymentId: string, updatePaymentDto: UpdatePaymentDto) {
+    const paymentToUpdate = await this.paymentRepository.getPaymentById(
+      paymentId,
+    );
+
+    if (
+      paymentToUpdate.status === PaymentStatus.OPEN &&
+      (!updatePaymentDto.status ||
+        updatePaymentDto.status !== PaymentStatus.PAID)
+    ) {
+      const { totalPaid, paymentDate, paymentMethod } = updatePaymentDto;
+
+      if (totalPaid || paymentDate || paymentMethod) {
+        throw new AppError(
+          'payment-service.updatePayment',
+          400,
+          'The properties totalPaid, paymentDate, and paymentMethod are not allowed when the payment status is OPEN',
+        );
+      }
+    }
+
+    if (updatePaymentDto.status === PaymentStatus.PAID) {
+      const requiredFields = ['totalPaid', 'paymentDate', 'paymentMethod'];
+      const missingFields = requiredFields.filter(
+        (field) => !updatePaymentDto[field],
+      );
+
+      if (missingFields.length > 0) {
+        throw new AppError(
+          'payment-service.updatePayment',
+          400,
+          `missing values for fields: ${missingFields.join(', ')}`,
+        );
+      }
+    }
+
     return this.paymentRepository.updatePayment(paymentId, updatePaymentDto);
   }
 }
