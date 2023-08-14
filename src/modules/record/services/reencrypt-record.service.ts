bea6f59d47c6e27b452e56b8feb51e77364804d1
rafittu <rafittu@outlook.com>
2023-08-14 17:55:24 -0300

    art: refact reencrypt records service

--- src/modules/record/services/reencrypt-record.service.ts
+++ src/modules/record/services/reencrypt-record.service.ts
@@ -11,46 +11,59 @@ export class ReencryptRecordsService {
     private recordRepository: IRecordRepository,
   ) {}
 
+  async decryptRecord(encryptedData: string): Promise<string> {
+    const cipherKey = Buffer.from(process.env.RECORD_CIPHER_KEY, 'hex');
+    const cipherIv = Buffer.from(process.env.RECORD_CIPHER_IV, 'hex');
+
+    const decipher = crypto.createDecipheriv(
+      process.env.RECORD_CIPHER_KEY,
+      cipherKey,
+      cipherIv,
+    );
+
+    let decryptedData = decipher.update(encryptedData, 'hex', 'utf8');
+    decryptedData += decipher.final('utf8');
+
+    return decryptedData;
+  }
+
+  async encryptRecord(data: string): Promise<string> {
+    const cipherKey = Buffer.from(
+      process.env.NEW_CIPHER_KEY || process.env.RECORD_CIPHER_KEY,
+      'hex',
+    );
+    const cipherIv = Buffer.from(
+      process.env.NEW_CIPHER_IV || process.env.RECORD_CIPHER_IV,
+      'hex',
+    );
+
+    const cipher = crypto.createCipheriv(
+      process.env.RECORD_CIPHER_KEY,
+      cipherKey,
+      cipherIv,
+    );
+
+    let encryptedData = cipher.update(data, 'utf8', 'hex');
+    encryptedData += cipher.final('hex');
+
+    return encryptedData;
+  }
+
   async execute(): Promise<string> {
     try {
       const records = await this.recordRepository.allRecords();
 
-      const updatedRecords = records.map((record) => {
-        const currentCipherKey = Buffer.from(
-          process.env.RECORD_CIPHER_KEY,
-          'hex',
-        );
-        const currentCipherIv = Buffer.from(
-          process.env.RECORD_CIPHER_IV,
-          'hex',
-        );
+      const updatedRecords = await Promise.all(
+        records.map(async (record) => {
+          const decryptedRecord = await this.decryptRecord(record.record);
+          const encryptedRecord = await this.encryptRecord(decryptedRecord);
 
-        const decipher = crypto.createDecipheriv(
-          process.env.RECORD_CIPHER_ALGORITHM,
-          currentCipherKey,
-          currentCipherIv,
-        );
-
-        let decryptedRecord = decipher.update(record.record, 'hex', 'utf8');
-        decryptedRecord += decipher.final('utf8');
-
-        const newCipherKey = Buffer.from(process.env.NEW_CIPHER_KEY, 'hex');
-        const newCipherIv = Buffer.from(process.env.NEW_CIPHER_IV, 'hex');
-
-        const cipher = crypto.createCipheriv(
-          process.env.RECORD_CIPHER_ALGORITHM,
-          newCipherKey,
-          newCipherIv,
-        );
-
-        let encryptedRecord = cipher.update(decryptedRecord, 'utf8', 'hex');
-        encryptedRecord += cipher.final('hex');
-
-        return {
-          ...record,
-          record: encryptedRecord,
-        };
-      });
+          return {
+            ...record,
+            record: encryptedRecord,
+          };
+        }),
+      );
 
       await this.recordRepository.updateAllRecords(updatedRecords);
 
@@ -59,7 +72,7 @@ export class ReencryptRecordsService {
       throw new AppError(
         'record-module.reencryptRecordsService',
         500,
-        error.code,
+        error.message,
       );
     }
   }
