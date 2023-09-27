42f27e9c69ef9a487edeefc7e0032f4b653ecaa8
rafittu <rafittu@outlook.com>
2023-09-27 14:47:19 -0300

    refact: new function to alma api requests

--- src/modules/user/repository/user.repository.ts
+++ src/modules/user/repository/user.repository.ts
@@ -25,47 +25,74 @@ import * as jwt from 'jsonwebtoken';
 export class UserRepository implements IUserRepository {
   constructor(private prisma: PrismaService) {}
 
-  private async almaPostRequest(path: string, body: object): Promise<AlmaUser> {
-    try {
-      const response = await axios.post(path, body);
-      return response.data;
-    } catch (error) {
-      const { status, code, message } = error.response.data.error;
-      throw new AppError(status, code, message);
-    }
-  }
+  // private async almaPostRequest(path: string, body: object): Promise<AlmaUser> {
+  //   try {
+  //     const response = await axios.post(path, body);
+  //     return response.data;
+  //   } catch (error) {
+  //     const { status, code, message } = error.response.data.error;
+  //     throw new AppError(status, code, message);
+  //   }
+  // }
 
-  private async almaGetRequest(
+  // private async almaGetRequest(
+  //   path: string,
+  //   accessToken: string,
+  // ): Promise<AlmaUserData> {
+  //   try {
+  //     const response = await axios.get(path, {
+  //       headers: {
+  //         Authorization: `Bearer ${accessToken}`,
+  //       },
+  //     });
+  //     return response.data;
+  //   } catch (error) {
+  //     const { status, code, message } = error.response.data.error;
+  //     throw new AppError(status, code, message);
+  //   }
+  // }
+
+  // private async almaPatchRequest(
+  //   path: string,
+  //   accessToken: string,
+  //   body: object,
+  // ): Promise<AlmaUserUpdated> {
+  //   try {
+  //     const response = await axios.patch(path, body, {
+  //       headers: {
+  //         Authorization: `Bearer ${accessToken}`,
+  //       },
+  //     });
+  //     return response.data;
+  //   } catch (error) {
+  //     const { status, code, message } = error.response.data.error;
+  //     throw new AppError(status, code, message);
+  //   }
+  // }
+
+  private async almaRequest<T>(
     path: string,
     accessToken: string,
-  ): Promise<AlmaUserData> {
+    method: 'post' | 'get' | 'patch',
+    body?: object,
+  ): Promise<T> {
     try {
-      const response = await axios.get(path, {
+      const config = {
         headers: {
           Authorization: `Bearer ${accessToken}`,
         },
-      });
-      return response.data;
-    } catch (error) {
-      const { status, code, message } = error.response.data.error;
-      throw new AppError(status, code, message);
-    }
-  }
+      };
 
-  private async almaPatchRequest(
-    path: string,
-    accessToken: string,
-    body: object,
-  ): Promise<AlmaUserUpdated> {
-    try {
-      const response = await axios.patch(path, body, {
-        headers: {
-          Authorization: `Bearer ${accessToken}`,
-        },
-      });
+      const response =
+        method === 'post'
+          ? await axios.post(path, body, config)
+          : method === 'patch'
+          ? await axios.patch(path, body, config)
+          : await axios.get(path, config);
+
       return response.data;
     } catch (error) {
-      const { status, code, message } = error.response.data.error;
+      const { status, code, message } = error.response?.data?.error || {};
       throw new AppError(status, code, message);
     }
   }
